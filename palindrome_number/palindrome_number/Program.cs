4afb4a8f059f35e85ee6b0ca158bbc19223b3e36
Paul Lowrance <tpaul.lowrance@gmail.com>
2022-09-20 05:33:03 -0500

    First Leetcode Submissions
    1 - Two Sum 
    9 - Palindrome Number

---
+++ palindrome_number/palindrome_number/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var s = new Solution();
+        
+        Console.WriteLine(s.IsPalindrome(121));
+        Console.WriteLine(s.IsPalindrome(-121));
+        Console.WriteLine(s.IsPalindrome(10));
+        Console.WriteLine(s.IsPalindrome(1000021));
+    }
+}
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x <= 0)
+            return false;
+        
+        var digits = new List<int>();
+        for (; x != 0; x /= 10)
+            digits.Add(x % 10);
+
+        var reversedArr = digits.ToArray();
+        Array.Reverse(reversedArr);
+
+        for (var i = 0; i < digits.Count; i++)
+        {
+            if (digits.ToArray()[i] != reversedArr[i])
+                return false;
+        }
+        return true;
+    }
+}
---
+++ two_sum/two_sum/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace two_sum;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var set1 = new[] { 2, 7, 11, 15 };
+        var target1 = 9;
+        var set2 = new[] { 3,2,4 };
+        var target2 = 6;
+        var set3 = new[] { 3,3 };
+        var target3 = 6;
+
+        var s = new Solution();
+
+        var solution1 = s.TwoSum(set1, target1);
+        var solution2 = s.TwoSum(set2, target2);
+        var solution3 = s.TwoSum(set3, target3);
+        
+        Console.WriteLine($"[{solution1[0]}, {solution1[1]}]");
+        Console.WriteLine($"[{solution2[0]}, {solution2[1]}]");
+        Console.WriteLine($"[{solution3[0]}, {solution3[1]}]");
+    }
+}
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var hashMap = new List<int[]>();
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var result = hashMap.FirstOrDefault(a => a[0] ==  nums[i]);
+            if (result != null)
+                return new []{result[1], i};
+            
+            hashMap.Add(new []{target - nums[i], i});
+        }
+
+        return new[] { -1, -1 };
+    }
+}
