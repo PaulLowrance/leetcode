1363d5e83ac6fb714a86d273774bedc2a1767955
Paul Lowrance <tpaul.lowrance@gmail.com>
2022-09-27 16:56:40 -0500

    adding the program file

---
+++ 4_median_of_two_sorted_arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MedianOfTwoSortedArrays;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var s1_arr1 = new[] { 1, 3 };
+        var s1_arr2 = new[] { 2 };
+        var s2_arr1 = new[] { 1, 2 };
+        var s2_arr2 = new[] { 3, 4 };
+        var s3_arr1 = new[] { 1, 2, 3, 7, 8 };
+        var s3_arr2 = new[] { 2, 3, 10, 12, 13, 14, 15 };
+        var s4_arr1 = new[] { 1, 3 };
+        var s4_arr2 = new[] { 2 };
+    }
+
+
+    public class Solution
+    {
+
+    }
+}
