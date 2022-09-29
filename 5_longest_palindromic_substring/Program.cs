98cd5c27ffb2ac58f8eed5ef0702a75b4726e1f5
Paul Lowrance <tpaul.lowrance@gmail.com>
2022-09-29 13:35:30 -0500

    Adds the basic app and the IsPalandrome method

---
+++ 5_longest_palindromic_substring/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace longest_palindromic_substring;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var s = new Solution();
+
+        Console.WriteLine($"is radar: {s.IsPalindrome("radar")}");
+        Console.WriteLine($"is boob: {s.IsPalindrome("boob")}");
+        Console.WriteLine($"is cat: {s.IsPalindrome("cat")}");
+        Console.WriteLine($"is dd: {s.IsPalindrome("dd")}");
+    }
+}
+
+public class Solution
+{
+    public string LongestPalindrome(string s)
+    {
+        return string.Empty;
+    }
+
+    public bool IsPalindrome(string s)
+    {
+        var beginIdx = 0;
+        var endIdx = s.Length - 1;
+
+        for (var i = 0; i <= Math.Floor(s.Length / 2.0); i++)
+        {
+            if (s[i] == s[endIdx])
+            {
+                endIdx--;
+                continue;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
